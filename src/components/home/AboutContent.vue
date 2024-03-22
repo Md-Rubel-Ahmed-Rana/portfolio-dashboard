6e8737f567a00d2dfd5541f1da1046683f978d01
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-03-23 00:30:51 +0600

    About data editing API integrated

--- src/components/home/AboutContent.vue
+++ src/components/home/AboutContent.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="p-10 shadow-md">
-    <h3 class="text-4xl font-bold mb-8">About content</h3>
-    <div>
-        <img :src="content?.image" alt="About image" class="w-60 h-48 p-3 border rounded-md mb-4">
-        <p class="text-lg font-semibold">Description 1: {{ content?.description1 }}</p>
-        <p class="text-lg font-semibold">Description 2: {{ content?.description2 }}</p>
-        <p class="mt-4">
-        <router-link to="/dashboard/utils/about-content/edit">
-            <button class="bg-blue-500 text-white px-5 py-2 rounded-md">Edit content</button>
-        </router-link>
-      </p>
+    <div class="p-10 shadow-md">
+        <h3 class="text-4xl font-bold mb-8">About content</h3>
+        <div>
+            <img :src="content?.image" alt="About image" class="w-60 h-48 p-3 border rounded-md mb-4">
+            <p class="mb-4"><span class="text-xl font-semibold">Description 1: </span> <span class="text-lg">{{ content?.description1}}</span>
+            </p>
+            <p class="mb-4"><span class="text-xl font-semibold">Description 2: </span> <span class="text-lg">{{content?.description2}}</span>
+            </p>
+            <p class="mt-4">
+                <router-link to="/dashboard/utils/about-content/edit">
+                    <button class="bg-blue-500 text-white px-5 py-2 rounded-md">Edit content</button>
+                </router-link>
+            </p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
--- src/components/home/EditAbout.vue
+++ src/components/home/EditAbout.vue
@@ -13,15 +13,31 @@
                     class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
             </div>
             <div class="mb-4">
-                <label for="image" class="block text-gray-700 w-40 h-40 border p-3 rounded-md font-semibold mb-2 text-lg">Image:</label>
-                <img :src="formData.image" alt="About section image">
+                <div class="block text-gray-700  font-semibold text-lg">
+                    <label for="image">Image:</label>
+                    <img class="w-48 h-40 border p-3 rounded-md" v-if="!isUploadImage" :src="formData.image"
+                        alt="Banner section image">
+                </div>
+                <input v-on:change="handleUploadImage" v-if="isUploadImage" accept="image/*" type="file" required
+                    id="image" name="image"
+                    class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500" />
+                <p>
+                    <span class="text-sm font-semibold" v-if="isUploading">Uploading Image. Please wait...</span>
+                </p>
+                <button v-if="!isUploadImage" @click="toggleImageUpload" type="button"
+                    class="bg-purple-500 text-white mt-4 py-2 px-3 rounded-md hover:bg-purple-700 focus:outline-none focus:ring focus:border-purple-500">
+                    Change image
+                </button>
+                <button v-if="isUploadImage" @click="toggleImageUpload" type="button"
+                    class="bg-purple-500 text-white mt-5 py-2 px-3 rounded-md hover:bg-purple-700 focus:outline-none focus:ring focus:border-purple-500">
+                    Cancel
+                </button>
             </div>
             <div class="mt-6">
-                <button 
-                type="submit"
-                class="bg-blue-600 text-white py-2 px-14 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-500">
+                <button type="submit"
+                    class="bg-blue-600 text-white py-2 px-14 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-500">
                     Save changes
-            </button>
+                </button>
             </div>
         </form>
     </div>
@@ -42,13 +58,27 @@ export default {
                 description1: "",
                 description2: "",
                 image: "",
-            }
+            },
+            isUploadImage: false,
+            isUploading: false,
         };
     },
     methods: {
-        ...mapActions(["getHomeData"]),
+        ...mapActions(["getHomeData", "uploadSingleImage", "updateAboutData"]),
         handleEditAboutData(){
-            console.log(this.id, this.formData);
+            this.updateAboutData({ id: this.id, data: this.formData })
+        },
+        async handleUploadImage(event) {
+            this.isUploading = true
+            const data = await this.uploadSingleImage(event.target.files[0]);
+            if (data?.url) {
+                this.isUploading = false
+                this.formData.image = data?.url
+                this.isUploadImage = false
+            }
+        },
+        toggleImageUpload() {
+            this.isUploadImage = !this.isUploadImage
         }
     },
     created() {
