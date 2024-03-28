05bc0afa35309e44c673698b112bfe7261dbf018
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-03-29 01:46:56 +0600

    Register completed

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,18 +1,25 @@
 import axios from "axios";
 import { IUser } from "./../types/user.type";
 import { ICommit } from "./../types/vuex.types";
+import router from "@/router";
 
 const baseApi = process.env.VUE_APP_BASE_API;
 
 export const userApi = {
   async registerUser({ commit }: ICommit, user: IUser) {
-    const res = await axios.post(`${baseApi}/user/register`);
+    const res = await axios.post(`${baseApi}/user/register`, user);
+    if (res?.data) {
+      router.push("/login");
+    }
   },
   async loginUser(
     { commit }: ICommit,
     credentials: { email: string; password: string }
   ) {
     const res = await axios.post(`${baseApi}/user/login`, credentials);
+    if (res?.data) {
+      router.push("/dashboard");
+    }
   },
   async getAllUsers({ commit }: ICommit) {
     const res = await axios.get(`${baseApi}/user`);
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -3,40 +3,70 @@
     <div class="text-2xl font-semibold mb-4">
       <h3 class="text-center">Register Now</h3>
     </div>
-    <div class="mb-4">
-      <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email">
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password">
-    </div>
-    <div class="flex items-center justify-center">
-      <button @click="submitForm" class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Register
-      </button>
-    </div>
+    <form @submit.prevent="handleRegister">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="name">
+          Name <span class="text-red-500">*</span>
+        </label>
+        <input v-model="formData.name" required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name" type="text" placeholder="Enter your name">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="email">
+          Email <span class="text-red-500">*</span>
+        </label>
+        <input v-model="formData.email" required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email" type="email" placeholder="Enter your email">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="phoneNumber">
+          Phone number <span class="text-xs">(Optional)</span>
+        </label>
+        <input v-model="formData.phoneNumber"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="phoneNumber" type="text" placeholder="Enter your phone number">
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-left text-sm font-bold mb-2" for="password">
+          Password <span class="text-red-500">*</span>
+        </label>
+        <input v-model="formData.password" required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="password" type="password" placeholder="Enter your password">
+      </div>
+      <div class="flex items-center justify-center">
+        <button
+          class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit">
+          Register
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import {mapActions} from "vuex"
 export default {
-  data() {
-    return {
-      email: '',
-      password: ''
+  methods: {
+    ...mapActions(["registerUser"]),
+    handleRegister() {
+      console.log("Clicked");
+      console.log(this.formData);
+      this.registerUser(this.formData)
     }
   },
-  methods: {
-    submitForm() {
-      // Handle form submission logic here
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        password: ""
+      }
     }
-  }
+  },
 }
 </script>
