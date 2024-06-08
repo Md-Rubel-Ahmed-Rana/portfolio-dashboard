d6d95b6fd8bc3c49c6da1344f72b9c1d2ce29d8a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-09 01:44:17 +0600

    Service CRUD operation completed

--- src/components/services/Services.vue
+++ src/components/services/Services.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="flex justify-between">
-      <h1 class="text-3xl font-semibold mb-4">Services</h1>
-      <p>
-        <router-link to="/dashboard/services/add-new-service">
-        <button class="bg-blue-600 px-5 py-2 rounded-md text-white">Add Service</button> 
-      </router-link> 
-      </p>
+  <div class="flex justify-between">
+    <h1 class="text-3xl font-semibold mb-4">Services</h1>
+    <p>
+      <router-link to="/dashboard/services/add-new-service">
+        <button class="bg-blue-600 px-5 py-2 rounded-md text-white">Add Service</button>
+      </router-link>
+    </p>
+  </div>
+  <div v-for="service in services" class="shadow-md p-5 rounded-md my-5 border">
+    <h5 class="text-xl font-semibold text-gray-600">{{ service.name }}</h5>
+    <p class="text-md text-gray-500">{{ service.description }}</p>
+    <div class="flex gap-3">
+      <button @click="handleDeleteService(service.id)"
+        class="bg-red-500 px-5 mt-5 py-2 rounded-md text-white">Delete</button>
+      <router-link :to="`/dashboard/services/edit/${service.id}`">
+        <button class="bg-blue-500 px-5 mt-5 py-2 rounded-md text-white">Update content</button>
+      </router-link>
     </div>
-    <div v-for="service in services" class="shadow-md p-5 rounded-md my-5 border" >
-        <h5 class="text-xl font-semibold text-gray-600">{{ service.name }}</h5>
-        <p class="text-md text-gray-500">{{ service.description }}</p>
-         <router-link :to="`/dashboard/services/edit/${service.id}`">
-        <button class="bg-blue-500 px-5 mt-5 py-2 rounded-md text-white">Update content</button> 
-      </router-link> 
-    </div>
-    <div>
-    </div>
+  </div>
+  <div>
+  </div>
 </template>
 
 <script>
@@ -26,7 +30,10 @@ export default {
     ...mapState(["services"])
   },
   methods: {
-    ...mapActions(["getServices"]),
+    ...mapActions(["getServices", "deleteService"]),
+    handleDeleteService(id){
+      this.deleteService({id})
+    }
   },
   created() {
     this.getServices()
--- src/router/service.routes.ts
+++ src/router/service.routes.ts
@@ -1,3 +1,5 @@
+import AddService from "@/components/services/AddService.vue";
+import EditService from "@/components/services/EditService.vue";
 import Services from "@/components/services/Services.vue";
 import { RouteRecordRaw } from "vue-router";
 
@@ -6,5 +8,16 @@ export const serviceRoutes: Array<RouteRecordRaw> = [
     path: "/dashboard/services",
     name: "Services",
     component: Services,
+  },
+  {
+    path: "/dashboard/services/add-new-service",
+    name: "Add New Services",
+    component: AddService,
+  },
+  {
+    path: "/dashboard/services/edit/:id",
+    name: "Edit Service",
+    component: EditService,
+    props: true,
   },
 ];
