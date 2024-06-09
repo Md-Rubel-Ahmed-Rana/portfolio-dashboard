583d6f119f95f8de0ecd9283a56809d2ebacd10a
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-09 13:14:09 +0600

    Feedback edit, delete and filtering completed

--- src/api/feedback.api.ts
+++ src/api/feedback.api.ts
@@ -1,12 +1,28 @@
-import { ICommit } from "@/types/vuex.types";
+import { IFeedback } from "./../types/feedback.type";
+import { ICommit, IDispatch } from "@/types/vuex.types";
 import axios from "axios";
 import { authHeader, baseApi } from ".";
 
 export const feedbackApi = {
-  async getFeedbacks({ commit }: ICommit) {
-    const res = await axios.get(`${baseApi}/feedback/all`, {
+  async getFeedbacks({ commit }: ICommit, { filter }: { filter: string }) {
+    const res = await axios.get(`${baseApi}/feedback/all?filter=${filter}`, {
       headers: authHeader,
     });
     commit("setFeedbacks", res?.data?.data);
+  },
+  async updateFeedback(
+    { dispatch }: IDispatch,
+    { id, payload }: { id: string; payload: IFeedback }
+  ) {
+    await axios.patch(`${baseApi}/feedback/update/${id}`, payload, {
+      headers: authHeader,
+    });
+    dispatch("getFeedbacks");
+  },
+  async deleteFeedback({ dispatch }: IDispatch, { id }: { id: string }) {
+    await axios.delete(`${baseApi}/feedback/delete/${id}`, {
+      headers: authHeader,
+    });
+    dispatch("getFeedbacks");
   },
 };
