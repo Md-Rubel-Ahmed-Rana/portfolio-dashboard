23935c8c1eb16cf8aaa5a8d71d66334ba926ff5f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-10 16:45:13 +0600

    Comment api integration completed

--- src/api/comment.api.ts
+++ src/api/comment.api.ts
@@ -3,8 +3,44 @@ import axios from "axios";
 import { authHeader, baseApi } from ".";
 
 export const commentApi = {
-  async getComments({ commit }: ICommit) {
-    const res = await axios.get(`${baseApi}/comment`, { headers: authHeader });
+  async getComments({ commit }: ICommit, { postId }: { postId: string }) {
+    let res;
+    if (postId && postId !== "All") {
+      res = await axios.get(`${baseApi}/comment?filter=${postId}`, {
+        headers: authHeader,
+      });
+    } else {
+      res = await axios.get(`${baseApi}/comment`, {
+        headers: authHeader,
+      });
+    }
+    commit("setComments", res?.data?.data);
+  },
+
+  async toggleSuspend(
+    { commit }: ICommit,
+    { id, suspend }: { id: string; suspend: boolean }
+  ) {
+    await axios.patch(
+      `${baseApi}/comment/suspend/${id}`,
+      { suspend },
+      {
+        headers: authHeader,
+      }
+    );
+    const res = await axios.get(`${baseApi}/comment`, {
+      headers: authHeader,
+    });
+    commit("setComments", res?.data?.data);
+  },
+
+  async deleteComment({ commit }: ICommit, { id }: { id: string }) {
+    await axios.delete(`${baseApi}/comment/delete/${id}`, {
+      headers: authHeader,
+    });
+    const res = await axios.get(`${baseApi}/comment`, {
+      headers: authHeader,
+    });
     commit("setComments", res?.data?.data);
   },
 };
