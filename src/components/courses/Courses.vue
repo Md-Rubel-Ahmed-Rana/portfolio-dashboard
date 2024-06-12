5783495c8756cd15577190908450f7966bab0ba9
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-12 14:08:47 +0600

    course pages responsive completed

--- src/components/courses/Courses.vue
+++ src/components/courses/Courses.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
-        <div class="flex justify-between">
-            <h1 class="text-3xl font-semibold mb-4">Courses</h1>
+        <div class="flex flex-col lg:flex-row justify-between">
+            <h1 class="text-3xl font-semibold mb-2 lg:mb-4">Courses</h1>
+
             <p>
                 <router-link to="/dashboard/courses/add-new-course">
-                    <button class="bg-blue-600 px-5 py-2 rounded-md text-white">Add Course</button>
+                    <button class="bg-blue-600 px-5 py-2 mb-4 lg:mb-0 rounded-md text-white">Add Course</button>
                 </router-link>
             </p>
+            <router-link to="/dashboard">
+                <button class="bg-blue-600 lg:hidden block px-5 text-white rounded-md my-2 py-1">Back</button>
+            </router-link>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div class="bg-white rounded-lg shadow-md p-4 border flex flex-col justify-between" v-for="course in courses"
-                :key="course.id">
+            <div class="bg-white rounded-lg shadow-md p-4 border flex flex-col justify-between"
+                v-for="course in courses" :key="course.id">
                 <div>
                     <img :src="course.image" alt="Course Image" class="w-full rounded-md h-48 object-top mb-4">
                     <h3 class="text-lg font-semibold mb-2">{{ course.name }}</h3>
