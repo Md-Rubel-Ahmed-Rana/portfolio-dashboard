36381e30c6c9a75f230b423a8a6c4abc23f1a4f3
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-12 15:47:25 +0600

    project pages responsiveness done

--- src/components/projects/EditProject.vue
+++ src/components/projects/EditProject.vue
@@ -1,51 +1,67 @@
 <template>
-  <div class="px-8 pb-8 bg-white shadow-md rounded-lg">
-    <h2 class="text-2xl font-semibold mb-4">Edit Project</h2>
+  <div class="lg:px-8 lg:pb-8 bg-white lg:shadow-md rounded-lg">
+    <div class="flex justify-between items-start lg:items-center">
+      <h2 class="text-2xl font-semibold mb-4">Edit Project</h2>
+      <router-link to="/dashboard/projects">
+        <button class="bg-blue-600 lg:hidden block px-5 text-white rounded-md mb-3 py-1">Back</button>
+      </router-link>
+    </div>
     <form @submit.prevent="submitForm">
       <div class="mb-4">
         <label for="name" class="block text-gray-700 font-semibold mb-2 text-lg">Project Name:</label>
-        <input type="text" v-model="formData.name" id="name" name="name" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+        <input type="text" v-model="formData.name" id="name" name="name"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
       </div>
       <div class="mb-4">
         <label for="description" class="block text-gray-700 font-semibold mb-2 text-lg">Description:</label>
-        <textarea v-model="formData.description" id="description" name="description" rows="4" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
+        <textarea v-model="formData.description" id="description" name="description" rows="4"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
       </div>
       <div class="mb-4">
         <h3 class="font-semibold mb-1 text-lg">Features</h3>
         <p class="text-sm mb-1">Note: Write feature by comma separated.</p>
-        <textarea v-model="formData.features " name="technologies" id="" cols="30" rows="5" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
+        <textarea v-model="formData.features " name="technologies" id="" cols="30" rows="5"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
       </div>
       <div class="mb-4">
         <h3 class="font-semibold mb-1 text-lg">Technologies</h3>
         <p class="text-sm mb-1">Note: Write tech by comma separated.</p>
-        <textarea v-model="formData.techStack " name="technologies" id="" cols="30" rows="5" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
+        <textarea v-model="formData.techStack " name="technologies" id="" cols="30" rows="5"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
       </div>
-      <div class="flex justify-between items-center gap-5 mb-4">
-        <div class="w-1/2">
+      <div class="flex justify-between flex-col lg:flex-row items-start lg:items-center gap-5 mb-4">
+        <div class="w-full lg:w-1/2">
           <label for="startDate" class="block text-gray-700 font-semibold mb-2 text-lg">Start Date:</label>
-          <input type="date" v-model="formData.projectLength.startDate" id="startDate" name="startDate" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+          <input type="date" v-model="formData.projectLength.startDate" id="startDate" name="startDate"
+            class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
         </div>
-        <div class="w-1/2">
+        <div class="w-full lg:w-1/2">
           <label for="endDate" class="block text-gray-700 font-semibold mb-2 text-lg">End Date:</label>
-          <input type="date" v-model="formData.projectLength.endDate" id="endDate" name="endDate" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+          <input type="date" v-model="formData.projectLength.endDate" id="endDate" name="endDate"
+            class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
         </div>
       </div>
       <div class="mb-4">
         <label for="sourceCode" class="block text-gray-700 font-semibold mb-2 text-lg">Repository link:</label>
-        <input type="text" v-model="formData.sourceCode" id="sourceCode" name="sourceCode" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+        <input type="text" v-model="formData.sourceCode" id="sourceCode" name="sourceCode"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
       </div>
       <div class="mb-4">
         <label for="liveLink" class="block text-gray-700 font-semibold mb-2 text-lg">Live Link:</label>
-        <input type="text" v-model="formData.liveLink" id="liveLink" name="liveLink" class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+        <input type="text" v-model="formData.liveLink" id="liveLink" name="liveLink"
+          class="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
       </div>
-      <div class="mb-4 flex items-center gap-2">
+      <div class="mb-4 flex flex-col lg:flex-row items-start lg:items-center gap-2">
         <label for="projectStatus" class="block text-gray-700 font-semibold mb-2 text-lg">Project Status:</label>
-        <select id="projectStatus" name="projectStatus" v-model="formData.projectStatus" class="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
-      <option v-for="status in projectStatus" :value="status" :key="status">{{ status }}</option>
-    </select>
+        <select id="projectStatus" name="projectStatus" v-model="formData.projectStatus"
+          class="border w-full border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
+          <option v-for="status in projectStatus" :value="status" :key="status">{{ status }}</option>
+        </select>
       </div>
       <div class="mt-6">
-        <button type="submit" class="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-500">Save Changes</button>
+        <button type="submit"
+          class="bg-blue-600 w-full text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-500">Save
+          Changes</button>
       </div>
     </form>
   </div>
--- src/components/projects/ProjectDetails.vue
+++ src/components/projects/ProjectDetails.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="">
-    <div class="rounded-lg shadow-md px-8 pb-6">
+    <div class="lg:rounded-lg lg:shadow-md lg:px-8 lg:pb-6">
+      <router-link to="/dashboard/projects">
+        <button class="bg-blue-600 lg:hidden block px-5 text-white rounded-md mb-3 py-1">Back</button>
+      </router-link>
       <h2 class="text-2xl font-semibold mb-4">{{ project?.name }}</h2>
       <p class="text-gray-700 mb-4">{{ project?.description }}</p>
       <div class="mb-4">
@@ -8,41 +11,43 @@
         <p><span class="font-semibold">Start Date:</span> {{ project?.projectLength?.startDate }}</p>
         <p><span class="font-semibold">End Date:</span> {{ project?.projectLength?.endDate }}</p>
       </div>
-      
+
       <div class="mb-4">
         <h3 class="text-lg font-semibold mb-2">Features</h3>
         <ul>
           <li v-for="(feature, index) in project?.features" :key="index" class="list-disc ml-4">{{ feature }}</li>
         </ul>
       </div>
-      
+
       <div class="mb-4">
         <h3 class="text-lg font-semibold mb-2">Technologies</h3>
-        <ul>
+        <ul class="flex flex-wrap gap-5">
           <li v-for="(tech, index) in project?.techStack" :key="index" class="list-disc ml-4">{{ tech }}</li>
         </ul>
       </div>
-      
+
       <div class="mb-4">
         <p><span class="font-semibold">Status:</span> {{ project?.projectStatus }}</p>
       </div>
-      
+
       <div class="mb-4">
         <a :href="project?.sourceCode" target="_blank" class="text-blue-600 hover:underline">Source Code</a>
         <span class="text-gray-400 mx-2">|</span>
         <a :href="project?.liveLink" target="_blank" class="text-blue-600 hover:underline">Live Link</a>
       </div>
-      
+
       <div class="mb-4">
-        <p><span class="font-semibold">Created At:</span> {{ project?.createdAt }}</p>
-        <p><span class="font-semibold">Last updated At:</span> {{ project?.updatedAt }}</p>
+        <p><span class="font-semibold">Created At:</span> {{ project?.createdAt?.slice(0, 10) }}</p>
+        <p><span class="font-semibold">Last updated At:</span> {{ project?.updatedAt?.slice(0, 10) }}</p>
       </div>
-      
+
       <div class="flex">
         <router-link :to="`/dashboard/projects/edit/${project?.id}`">
-          <button class="bg-blue-600 px-10 py-2 rounded-md text-white mr-4 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">Edit</button>
+          <button
+            class="bg-blue-600 px-10 py-2 rounded-md text-white mr-4 hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">Edit</button>
         </router-link>
-        <button @click="deleteProject(project?.id)" class="bg-red-600 px-10 py-2 rounded-md text-white hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-300">Delete</button>
+        <button @click="deleteProject(project?.id)"
+          class="bg-red-600 px-10 py-2 rounded-md text-white hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-300">Delete</button>
       </div>
     </div>
   </div>
